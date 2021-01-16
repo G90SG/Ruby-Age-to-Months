654fb7f7c871b89e14a203e02d3dbc85f77ea458
G90SG <71718345+G90SG@users.noreply.github.com>
2021-01-16 16:02:01 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+# create the variables
 name = ""
 years = 0
 M_PS = 12 #a constant 
@@ -10,10 +11,11 @@ print "What's your name? "
 name = gets
 name = name.chomp.capitalize! 
 #or name = gets.chomp 
-
+#obtain input from the user, asking they age in years
 print "How many years old are you? "
-years = gets.to_i #gets to_i converts string to integer
+years = gets.to_i 
+#gets to_i converts string to integer
 months = years*M_PS
-
+#print the users name, years olf and advise how many months this is
 print "#{name} at #{years} years old,"\
 " you are #{months} months old"
