5a4c7ea39605fa2ce4a66f6f9f48333a97c436e7
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 18:44:35 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
-# create the variables
+# Create the variables for Name (string) and Year (int), assigning them with a data type
 name = ""
 years = 0
-M_PS = 12 #a constant 
+M_PS = 12 # a constant (12 months)
 
-#output variable 
+# Output variable 
 months = 0 
 
-#processing 
+# Asking for the users name 
 print "What's your name? "
+# Calling .chomp method to remove the newline character at the end before calling .capitalize! to capitalise the entry
 name = gets
-name = name.chomp.capitalize! 
-#or name = gets.chomp 
-#obtain input from the user, asking they age in years
-print "How many years old are you? "
+name = gets.chomp.capitalize! 
+# Obtain input from the user, asking their age in years
+print "How many years old are you #{name}? "
 years = gets.to_i 
-#gets to_i converts string to integer
+# Gets to_i - converts string to integer
 months = years*M_PS
-#print the users name, years olf and advise how many months this is
-print "#{name} at #{years} years old,"\
+# Print years old and advise how many months this is
+print "At #{years} years old,"\
 " you are #{months} months old"
