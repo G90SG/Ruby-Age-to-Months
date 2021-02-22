986e623df396d9f4dbff8a048a413c15d9697d21
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 11:46:53 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,14 +6,13 @@ M_PS = 12 # a constant (12 months)
 months = 0 
 # Asking for the users name 
 print "What's your name? "
-# Calling .chomp method to remove the newline character at the end before calling .capitalize! to capitalise the entry
-name = gets.capitalize! 
-# Obtain input from the user, asking their age in years
-print "How many years old are you #{name.chomp}? "
+# Calling .chomp method to remove the newline character at the end calling .capitalize! to capitalise the entry
+name = gets.chomp.capitalize! 
+# Obtain input from the user, asking their age in years using the input assigned to the 'name' variable - 
+print "How many years old are you #{name}? "
 years = gets.to_i 
 # Gets to_i - converts string to integer
 months = years*M_PS
 # Print years old and advise how many months this is
-print "#{name.chomp}, at #{years} years old,"\
+print "#{name}, at #{years} years old,"\
 " you are #{months} months old"
-
